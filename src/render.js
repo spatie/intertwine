614893a8556957743cb213ca910100557d48a27c
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2020-07-23 16:15:28 +0200

    Fix imports/exports

--- src/render.js
+++ src/render.js
@@ -1,6 +1,6 @@
-import path from "path";
-import nunjucks from "nunjucks";
-import { markdown } from "./util";
+const path = require("path");
+const nunjucks = require("nunjucks");
+const { markdown } = require("./util");
 
 const renderer = new nunjucks.Environment(
   new nunjucks.FileSystemLoader([
@@ -11,6 +11,8 @@ const renderer = new nunjucks.Environment(
 
 renderer.addFilter("markdown", markdown);
 
-export function render(page, site) {
+function render(page, site) {
   return renderer.render("page.html", { page, site });
 }
+
+module.exports.render = render;
